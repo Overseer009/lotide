595d173a0b2a4ced87ea2691cd12732bd144d7b5
Overseer009 <sacha.sunnasy@gmail.com>
2021-04-28 13:02:09 -0400

    completed the countOnly function for lotide

--- middle.js
+++ middle.js
@@ -2,9 +2,9 @@ const middle = function(array) {
   const middleNum = Math.round(array.length / 2);
   const length = array.length;
   if (length <= 2) return [];
-  if (length % 2 === 0) {
-    return [array[middleNum - 1], array[middleNum]];
-  } else return [array[middleNum - 1]];
+  if (length % 2 !== 0) {
+    return [array[middleNum - 1]];
+  } else return [array[middleNum - 1], array[middleNum]];
 };
 
 const assertArraysEqual = function(givenArr, expectedArr) {
