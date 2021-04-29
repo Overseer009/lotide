a1641a20780edf1959648301ac4769729dc9a57b
Overseer009 <sacha.sunnasy@gmail.com>
2021-04-29 15:04:04 -0400

    forgot to apply my assertArraysEqual function, corrected and completed

--- takeUntil.js
+++ takeUntil.js
@@ -9,12 +9,30 @@ const takeUntil = (array, cb) => {
   }
 };
 
+const assertArraysEqual = function(givenArr, expectedArr) {
+  if (eqArrays(givenArr, expectedArr) === true) {
+    console.log('🙌 🙌 🙌 Hazza! It is a match!');
+  } else {
+    console.log('🤌 🤌 🤌  why you not match?');
+  }
+};
+
+const eqArrays = function(firstArray, secondArray) {
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  }
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
+assertArraysEqual(takeUntil(data1, x => x < 0), [ 1, 2, 5, 7, 2 ]);
 
 console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
+assertArraysEqual(takeUntil(data2, x => x === ','), [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
