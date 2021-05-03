369fc3d04c8be7d3004e4a9b0b5206d6e4c53e7a
Overseer009 <sacha.sunnasy@gmail.com>
2021-05-03 10:01:24 -0400

    completed the lotide refactor example

--- head.js
+++ head.js
@@ -1,13 +1,7 @@
-const head = function(array) {
-  return array[0]
-}
+const assertEqual = require('./assertEqual');
 
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`👎 👎 👎 Assertion Fail: ${actual} !== ${expected}`);
-  } else {
-    console.log(`👌 👌 👌 Assertion Passed: ${actual} === ${expected}`);
-  }
+const head = function(array) {
+  return array[0];
 };
 
 assertEqual(head([5,6,7]), 5);
