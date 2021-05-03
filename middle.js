6aa5240b48d4bbb1b7d67d097a22d28d19c1b0dd
Overseer009 <sacha.sunnasy@gmail.com>
2021-05-03 12:02:53 -0400

    refactored the middle.js and created the test.js for it as well

--- middle.js
+++ middle.js
@@ -7,26 +7,4 @@ const middle = function(array) {
   } else return [array[middleNum - 1], array[middleNum]];
 };
 
-const assertArraysEqual = function(givenArr, expectedArr) {
-  if (eqArrays(givenArr, expectedArr) === true) {
-    console.log('🙌 🙌 🙌 Hazza! It is a match!');
-  } else {
-    console.log('🤌 🤌 🤌  why you not match?');
-  }
-};
-
-const eqArrays = function(firstArray, secondArray) {
-  if (firstArray.length !== secondArray.length) {
-    return false;
-  }
-  for (let i = 0; i < firstArray.length; i++) {
-    if (firstArray[i] !== secondArray[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3, 4, 5]),  [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
