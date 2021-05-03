e0db976478bf8b982cea3cc2b75788d939799f41
Overseer009 <sacha.sunnasy@gmail.com>
2021-05-03 10:16:27 -0400

    refactored the tail.js and created the test.js for it aswell

--- tail.js
+++ tail.js
@@ -1,3 +1,5 @@
+const assertEqual = require('./assertEqual');
+
 const tail = function(array) {
   let tailArray = [];
   for (let i = 1; i < array.length; i++) {
@@ -6,14 +8,4 @@ const tail = function(array) {
   return tailArray;
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`👎 👎 👎 Assertion Fail: ${actual} !== ${expected}`);
-  } else {
-    console.log(`👌 👌 👌 Assertion Passed: ${actual} === ${expected}`);
-  }
-};
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+module.exports = tail;
